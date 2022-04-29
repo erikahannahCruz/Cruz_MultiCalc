392909d9f2ac09bace6972d85208385abf85d518
p1p1pupu <11erikacruz11@gmail.com>
2022-04-29 17:32:25 +0800

    Connected Geometry fragments to bottom navigation

--- app/src/main/java/com/example/cruz_multicalc/GeometryMenu.java
+++ app/src/main/java/com/example/cruz_multicalc/GeometryMenu.java
@@ -1,14 +1,53 @@
 package com.example.cruz_multicalc;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
 
-public class GeometryMenu extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class GeometryMenu extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavGeometry;
+
+    gCube cube = new gCube();
+    gCylinder cylinder = new gCylinder();
+    gSphere sphere = new gSphere();
+    gCone cone = new gCone();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_geometry);
+
+        bottomNavGeometry = findViewById(R.id.bottomNavGeometry) ;
+        bottomNavGeometry.setOnNavigationItemSelectedListener(this);
+        bottomNavGeometry.setSelectedItemId(R.id.sphere);
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cube:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentGeometry, cube).commit();
+                return true;
+
+            case R.id.cylinder:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentGeometry, cylinder).commit();
+                return true;
+
+            case R.id.cone:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentGeometry, cone).commit();
+                return true;
+
+            case R.id.sphere:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentGeometry, sphere).commit();
+                return true;
+
+        }
+        return false;
+    }
+
 }
