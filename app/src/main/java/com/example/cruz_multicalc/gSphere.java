df184ef792db2fa21d6c82a811c1f9b79569c75e
p1p1pupu <11erikacruz11@gmail.com>
2022-04-29 14:45:31 +0800

    Created 4 fragments each for physics and geometry

---
+++ app/src/main/java/com/example/cruz_multicalc/gSphere.java
@@ -0,0 +1,24 @@
+package com.example.cruz_multicalc;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class gSphere extends Fragment {
+
+
+    public gSphere() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_g_sphere, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/cruz_multicalc/pAcceleration.java
@@ -0,0 +1,25 @@
+package com.example.cruz_multicalc;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class pAcceleration extends Fragment {
+
+
+
+    public pAcceleration() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_p_acceleration, container, false);
+    }
+}
