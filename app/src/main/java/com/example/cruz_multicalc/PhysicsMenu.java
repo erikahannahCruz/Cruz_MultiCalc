aea4d53306aa51d90c2bd1a6159d41a9a3e87507
p1p1pupu <11erikacruz11@gmail.com>
2022-04-29 16:23:10 +0800

    Connected Physics fragments to bottom navigation

--- app/src/main/java/com/example/cruz_multicalc/PhysicsMenu.java
+++ app/src/main/java/com/example/cruz_multicalc/PhysicsMenu.java
@@ -2,12 +2,52 @@ package com.example.cruz_multicalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class PhysicsMenu extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PhysicsMenu extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavPhysics;
+
+    pDensity density = new pDensity();
+    pAcceleration acceleration = new pAcceleration();
+    pSpeed speed = new pSpeed();
+    pWeight weight = new pWeight();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_physics);
+
+        bottomNavPhysics = findViewById(R.id.bottomNavPhysics) ;
+        bottomNavPhysics.setOnNavigationItemSelectedListener(this);
+        bottomNavPhysics.setSelectedItemId(R.id.density);
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.speed:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentPhysics, speed).commit();
+                return true;
+
+            case R.id.density:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentPhysics, density).commit();
+                return true;
+
+            case R.id.acceleration:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentPhysics, acceleration).commit();
+                return true;
+
+            case R.id.weight:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragmentPhysics, weight).commit();
+                return true;
+
+        }
+        return false;
+    }
+
 }
